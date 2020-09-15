7b0b2d524bf3b277afa6b456f9d6a1a701e2a63f
Kerolos <kerolos.sss@gmail.com>
2020-09-15 13:28:43 +0200

    storing jwt sign in

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://4a36e1d13380.ngrok.io',
+  baseURL: 'http://77e3a885c7cb.ngrok.io',
 });
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,27 +1,32 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
+    case 'signup':
+      return { errorMessage: '', token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = dispatch => {
-  return async ({ email, password }) => {
-    try {
-      const response = await trackerApi.post('/signup', { email, password });
-      console.log(response.data);
-    } catch (err) {
-      dispatch({
-        type: 'add_error',
-        payload: 'Something went wrong with sign up'
-      });
-    }
-  };
+const signup = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signup', payload: response.data.token });
+
+    navigate('TrackList');
+  } catch (err) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign up'
+    });
+  }
 };
 
 const signin = dispatch => {
@@ -41,5 +46,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false, errorMessage: '' }
+  { token: null, errorMessage: '' }
 );
